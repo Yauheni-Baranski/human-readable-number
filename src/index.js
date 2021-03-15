7f6a92dc4f88b610204cd5d9b29b4cd5d74bc70a
Yauheni-Baranski <yauheni.baranski@gmail.com>
2021-03-16 01:21:49 +0300

    init: human-readable-number

--- src/index.js
+++ src/index.js
@@ -1,3 +1,61 @@
-module.exports = function toReadable (number) {
-  
-}
+const singlesWords = [
+    "",
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine",
+    "ten",
+    "eleven",
+    "twelve",
+    "thirteen",
+    "fourteen",
+    "fifteen",
+    "sixteen",
+    "seventeen",
+    "eighteen",
+    "nineteen",
+];
+const tensWords = [
+    "",
+    "ten",
+    "twenty",
+    "thirty",
+    "forty",
+    "fifty",
+    "sixty",
+    "seventy",
+    "eighty",
+    "ninety",
+];
+
+module.exports = function toReadable(number) {
+    if (number === 0) {
+        return "zero";
+    }
+    let hundreds = parseInt(number / 100);
+    let singles = number % 100;
+    let res = [];
+    if (hundreds) {
+        res.push(`${singlesWords[hundreds]} hundred`);
+    }
+    if (singles) {
+        if (singles < 20) {
+            res.push(`${singlesWords[singles]}`);
+        } else {
+            let tens = parseInt(singles / 10);
+            if (tens) {
+                res.push(`${tensWords[tens]}`);
+            }
+            singles = singles % 10;
+            if (singles) {
+                res.push(`${singlesWords[singles]}`);
+            }
+        }
+    }
+    return res.join(" ");
+};
